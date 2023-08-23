5ff0b8af3bc3a945e0afcdbb92e98995d19e77d2
Alfred Neumayer <dev.beidl@gmail.com>
2023-08-23 20:24:27 +0200

    Build Simulator framework as well

--- build.sh
+++ build.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-cmake -G Xcode -B build \
+cmake -G Xcode -B build-iphoneos \
     -DCMAKE_SYSTEM_NAME=iOS \
     -DCMAKE_Swift_COMPILER_FORCED=true \
     -DCMAKE_OSX_DEPLOYMENT_TARGET=11.0
 
-cd build
+cmake -G Xcode -B build-iphonesimulator \
+    -DCMAKE_SYSTEM_NAME=iOS \
+    -DCMAKE_OSX_SYSROOT=iphonesimulator \
+    -DCMAKE_Swift_COMPILER_FORCED=true \
+    -DCMAKE_OSX_DEPLOYMENT_TARGET=11.0
+
+cd build-iphoneos
 xcodebuild
+cd ..
+
+cd build-iphonesimulator
+xcodebuild
+cd ..
